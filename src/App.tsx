ec5bfdcfb942b4c5863f70ab556582c549c2ce1e
Arrowana <Arrowana@users.noreply.github.com>
2023-07-25 16:56:49 +1000

    Add anchor, js-sha256 and direct crypto webapi sha256 to compare

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { sha256 } from "js-sha256";
+import { BorshAccountsCoder } from "@coral-xyz/anchor";
 
 function App() {
+  const [webApiDiscriminator, setWebApiDiscriminator] = useState<Uint8Array>();
+
+  useEffect(() => {
+    (async () => {
+      setWebApiDiscriminator(
+        new Uint8Array(
+          await crypto.subtle.digest(
+            "sha-256",
+            new TextEncoder().encode("account:Organisation")
+          )
+        ).slice(0, 8)
+      );
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Direct js-sha256 digest:{" "}
+          {sha256.digest("account:Organisation").slice(0, 8)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Anchor discriminator{" "}
+          {BorshAccountsCoder.accountDiscriminator("Organisation")}
+        </p>
+        <p>Direct js-sha256 digest: {webApiDiscriminator}</p>
+        <p>
+          Expected discriminator:{" "}
+          {new Uint8Array([100, 235, 183, 55, 240, 174, 86, 191])}
+        </p>
       </header>
     </div>
   );
